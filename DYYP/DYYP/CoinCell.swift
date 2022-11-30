478e28b4d2ad091243b350217858b4ce9194c6d0
dustinsherwood <102439006+dustinsherwood@users.noreply.github.com>
2022-11-29 20:21:14 -0500

    purchase/sell buttons on wallet

--- DYYP/DYYP/CoinCell.swift
+++ DYYP/DYYP/CoinCell.swift
@@ -8,11 +8,54 @@
 import UIKit
 
 class CoinCell: UITableViewCell {
-
+    var buying = true
+    
     @IBOutlet weak var coinLogoImage: UIImageView!
     @IBOutlet weak var coinNameLabel: UILabel!
     @IBOutlet weak var informationLabel: UILabel!
     @IBOutlet weak var ownedLabel: UILabel!
+    
+    @IBAction func purchaseButton(_ sender: Any) {
+        buying = true
+        dollarsign.isHidden = false
+        confirmButtonOutlet.isHidden = false
+        confirmButtonOutlet.isEnabled = true
+        purchaseAmount.isEnabled = true
+        purchaseAmount.isHidden = false
+        purchaseAmount.becomeFirstResponder()
+        
+    }
+    
+    @IBAction func sellButton(_ sender: Any) {
+        buying = false
+        dollarsign.isHidden = false
+        confirmButtonOutlet.isHidden = false
+        confirmButtonOutlet.isEnabled = true
+        purchaseAmount.isEnabled = true
+        purchaseAmount.isHidden = false
+        purchaseAmount.becomeFirstResponder()
+    }
+    
+    @IBAction func confirmButton(_ sender: Any) {
+        confirmButtonOutlet.isHidden = true
+        confirmButtonOutlet.isEnabled = false
+        dollarsign.isHidden = true
+        if buying == false{
+            purchaseAmount.text = "-" + purchaseAmount.text!
+        }
+        print((coinNameLabel.text ?? "coin") + (purchaseAmount.text ?? "0.00"))
+        purchaseAmount.text = ""
+        purchaseAmount.isEnabled = false
+        purchaseAmount.isHidden = true
+        
+    }
+    
+    @IBOutlet weak var purchaseAmount: UITextField!
+    
+    @IBOutlet weak var confirmButtonOutlet: UIButton!
+    
+    
+    @IBOutlet weak var dollarsign: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
